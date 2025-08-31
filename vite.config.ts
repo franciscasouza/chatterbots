9175923530f50cc952eca4c74bd6195796e2f52f
Francisca Souza <franciscasouza@ceet.secti.es.gov.br>
2025-08-31 15:45:22 -0300

    projeto funcionando perfeitamente

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,19 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      plugins: [
-        react(),
-        basicSsl()
-      ],
+      plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+        "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+          "@": path.resolve(__dirname, "."),
+        },
       },
       server: {
-        host: true
-      }
+        host: true,
+        allowedHosts: true,
+      },
     };
 });
